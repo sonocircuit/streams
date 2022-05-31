fe7bfe2b890912c3351ea6845cc3dcca515235a1
sonoCircuits <49308219+sonoCircuits@users.noreply.github.com>
2022-05-31 14:40:45 +0200

    Update halfsync.lua

--- lib/halfsync.lua
+++ lib/halfsync.lua
@@ -13,33 +13,33 @@ warble.slope = 0
 warble.active = false
 
 function halfsync.init()
-	audio.level_cut(1.0)
-	audio.level_adc_cut(1)
-	audio.level_eng_cut(1)
+  audio.level_cut(1.0)
+  audio.level_adc_cut(1)
+  audio.level_eng_cut(1)
   softcut.level(1,1.0)
   softcut.level_slew_time(1,0.25)
-	softcut.level_input_cut(1, 1, 1.0)
-	softcut.level_input_cut(2, 1, 1.0)
-	softcut.pan(1, 0)
+  softcut.level_input_cut(1, 1, 1.0)
+  softcut.level_input_cut(2, 1, 1.0)
+  softcut.pan(1, 0)
 
   softcut.play(1, 1)
-	softcut.rate(1, 1)
+  softcut.rate(1, 1)
   softcut.rate_slew_time(1,0)
-	softcut.loop_start(1, 1)
-	softcut.loop_end(1, 1)
-	softcut.loop(1, 1)
-	softcut.fade_time(1, 0.1)
-	softcut.rec(1, 1)
-	softcut.rec_level(1, 1)
-	softcut.pre_level(1, 0.75)
-	softcut.position(1, 1)
-	softcut.enable(1, 1)
+  softcut.loop_start(1, 1)
+  softcut.loop_end(1, 1)
+  softcut.loop(1, 1)
+  softcut.fade_time(1, 0.1)
+  softcut.rec(1, 1)
+  softcut.rec_level(1, 1)
+  softcut.pre_level(1, 0.75)
+  softcut.position(1, 1)
+  softcut.enable(1, 1)
 
-	softcut.post_filter_dry(1, 0.125)
-	softcut.post_filter_fc(1, 1200)
-	softcut.post_filter_lp(1, 0)
-	softcut.post_filter_bp(1, 1.0)
-	softcut.post_filter_rq(1, 2.0)
+  softcut.post_filter_dry(1, 0.125)
+  softcut.post_filter_fc(1, 1200)
+  softcut.post_filter_lp(1, 0)
+  softcut.post_filter_bp(1, 1.0)
+  softcut.post_filter_rq(1, 2.0)
 
   params:add_control("delay_level", "delay level", controlspec.new(0, 1, 'lin' , 0, 0, ""))
   params:set_action("delay_level", function(x) softcut.level(1, x) d_refresh() end)
@@ -53,19 +53,19 @@ function halfsync.init()
   params:add_control("delay_feedback", "delay feedback", controlspec.new(0, 1.0, 'lin', 0 , 0.30 ,""))
   params:set_action("delay_feedback", function(x) softcut.pre_level(1, x) set_freez(x) d_refresh() end)
 
-	params:add_separator("wow & flutter")
+  params:add_separator("wow & flutter")
 
-	params:add_number("warble_amount", "amount", 0, 100, 0, function(param) return (param:get().." %") end)
+  params:add_number("warble_amount", "amount", 0, 100, 0, function(param) return (param:get().." %") end)
 
-	params:add_number("warble_depth", "depth", 0, 100, 12, function(param) return (param:get().." %") end)
+  params:add_number("warble_depth", "depth", 0, 100, 12, function(param) return (param:get().." %") end)
 
-	params:add_control("warble_freq","speed", controlspec.new(1.0, 10.0, "lin", 0.1, 6.0, ""))
-	params:set_action("warble_freq", function(val) warble.freq = val * 1.2 end)
+  params:add_control("warble_freq","speed", controlspec.new(1.0, 10.0, "lin", 0.1, 6.0, ""))
+  params:set_action("warble_freq", function(val) warble.freq = val * 1.2 end)
 
   clock.run(clock_update_rate)
 
   warbletimer = metro.init(function() make_warble() end, 0.1, -1)
-	warbletimer:start()
+  warbletimer:start()
 
 end
 
@@ -82,19 +82,19 @@ function clock_update_rate()
 end
 
 function set_freez(x)
-	local fb = x
-	if fb == 1.0 then
-		softcut.rec_level(1, 0)
-	else
-		softcut.rec_level(1, 1)
-	end
+  local fb = x
+  if fb == 1.0 then
+    softcut.rec_level(1, 0)
+  else
+    softcut.rec_level(1, 1)
+  end
 end
 
 function set_del_rate()
-	local tempo = params:get("clock_tempo")
-	local del_rate = ((60 / tempo) * div_options[params:get("delay_length")] * 4)
-	local set_rate = 1 + del_rate - (del_rate * (params:get("delay_length_ft") / 100))
-	softcut.loop_end(1, set_rate)
+  local tempo = params:get("clock_tempo")
+  local del_rate = ((60 / tempo) * div_options[params:get("delay_length")] * 4)
+  local set_rate = 1 + del_rate - (del_rate * (params:get("delay_length_ft") / 100))
+  softcut.loop_end(1, set_rate)
 end
 
 function make_warble()
